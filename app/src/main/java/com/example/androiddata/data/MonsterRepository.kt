2814287e08275c7cd911bf4a1bf543e577b594d1
Aleksandr Zdrachek <azdrachek@sent.at>
2020-11-24 16:54:08 +0300

    Read and write from room database

--- app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
+++ app/src/main/java/com/example/androiddata/data/MonsterRepository.kt
@@ -6,6 +6,7 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.net.ConnectivityManager
 import android.util.Log
+import android.widget.Toast
 import androidx.annotation.WorkerThread
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.MutableLiveData
@@ -18,26 +19,42 @@ import com.squareup.moshi.Types
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MonsterRepository(val app: Application) {
 
     val monsterData = MutableLiveData<List<Monster>>()
+    val monsterDao = MonsterDatabase.getDatabase(app).monsterDao()
 
     init {
-        val data = readDataFormCache()
-        if (data.isEmpty()) {
-            refreshDataFromWeb()
-        } else {
-            monsterData.value = data
-            Log.i(LOG_TAG, "Using local data")
+//        val data = readDataFormCache()
+//        if (data.isEmpty()) {
+//            refreshDataFromWeb()
+//        } else {
+//            monsterData.value = data
+//            Log.i(LOG_TAG, "Using local data")
+//        }
+        CoroutineScope(Dispatchers.IO).launch {
+            val data = monsterDao.getAll()
+            if (data.isEmpty()) {
+                callWebService()
+            } else {
+                monsterData.postValue(data)
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(app, "Data from DB", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
     @WorkerThread
     suspend fun callWebService() {
         if (networkAvailable()) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(app, "Remote data", Toast.LENGTH_SHORT).show()
+            }
             val retrofit = Retrofit.Builder()
                 .baseUrl(WEB_SERVICE_URL)
                 .addConverterFactory(MoshiConverterFactory.create())
@@ -47,6 +64,9 @@ class MonsterRepository(val app: Application) {
             monsterData.postValue(serviceData)
             Log.i(LOG_TAG, "Calling web service")
             saveDataToCache(serviceData)
+
+            monsterDao.deleteAll()
+            monsterDao.insertMonsters(serviceData)
         }
     }
 
@@ -69,24 +89,24 @@ class MonsterRepository(val app: Application) {
                 app,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE
             ) == PackageManager.PERMISSION_GRANTED
-        ) {}
-
+        ) {
             val adapter: JsonAdapter<List<Monster>> = moshiJsonAdapter()
             val json = adapter.toJson(monsterData)
 
             FileHelper.saveTextToFile(app, json)
         }
+    }
 
-        private fun readDataFormCache(): List<Monster> {
-            FileHelper.readTextFile(app).let {
-                val adapter = moshiJsonAdapter()
-                return if (it == null) emptyList() else adapter.fromJson(it) ?: emptyList()
-            }
-        }
-
-        private fun moshiJsonAdapter(): JsonAdapter<List<Monster>> {
-            val moshi = Moshi.Builder().build()
-            val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
-            return moshi.adapter(listType)
+    private fun readDataFormCache(): List<Monster> {
+        FileHelper.readTextFile(app).let {
+            val adapter = moshiJsonAdapter()
+            return if (it == null) emptyList() else adapter.fromJson(it) ?: emptyList()
         }
     }
+
+    private fun moshiJsonAdapter(): JsonAdapter<List<Monster>> {
+        val moshi = Moshi.Builder().build()
+        val listType = Types.newParameterizedType(List::class.java, Monster::class.java)
+        return moshi.adapter(listType)
+    }
+}
--- app/src/main/java/com/example/androiddata/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/example/androiddata/ui/splash/SplashFragment.kt
@@ -21,10 +21,6 @@ class SplashFragment : Fragment() {
         Navigation.findNavController(requireActivity(), R.id.navHost)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
